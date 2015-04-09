172d23b2304d56ca717c4c092bdb9e5a36011d30
Quiran Storey <qstorey@gmail.com>
2015-04-09 23:08:45 +0200

    Refactor the project, again
    This commit attempts to improve the S3Proxy package.
    The style was chosen after looking at other open source go projects like
    Hugo and Consul.

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+        "github.com/qstorey/S3Proxy/S3Proxy"
+        "net/http"
+)
+
+func main() {
+        // Load the default options
+        S3Proxy.LoadDefaultOptions()
+        // Run the startup configuration
+        S3Proxy.Configure()
+        // Connect to S3
+        S3Proxy.S3Connect()
+        // Set up the routing
+        mux := S3Proxy.SetUpRoutes()
+        http.Handle("/", mux)
+        // Start the HTTP serer
+        http.ListenAndServe(S3Proxy.Options.BindAddress, nil)
+}
